e48071f1b5c553c7fe33a2ce91a6b149bb2496d0
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-01-08 14:16:52 +0000

    Updated palaeoPull, added LFI mammal scripts

--- functions/paleoPull.R
+++ functions/paleoPull.R
@@ -3,12 +3,35 @@ library(stringr)
 library(plyr)
 library(RJSONIO)
 
-names <- "Orycteropus"
-url <- "http://paleobiodb.org/data1.1/taxa/list.json?"
-names2 <- paste("name=", paste(str_replace_all(names, " ", "+"), collapse = "|", sep = ""), sep = "")
-query <- paste(compact(list(url, names2)), collapse = "")
-#search via API
-fromJSON(query)
+taxaNames <- function(name, return.method, full.details = TRUE, limit = 10){
+  # Search taxomnomic names in paleoDB
+  #
+  # Arguments
+  #  name: string of name to be searched
+  #  return.method: string specifying the return method (single, children, parents or auto)
+  #  full.details: return all details or not (Boolean)
+  #  limit: number of returned records (auto method only)
+  #
+  # Return
+  #  Parsed JSON for the R envrionment
+  # TODO: create dataframe of JSON object, add full detail bool, allow IDs to be used,
+  #  find safe url package in R (e.g. what if there is non-url friendly characters in name
+  #  given?)
+  url <- "http://paleobiodb.org/data1.1/taxa/"
+  if (return.method == "single") {
+    url <- paste0(url, "single.json?name=", name)
+  } else if (return.method == "children") {
+    url <- paste0(url, "list.json?name=", name, "&rel=all_children")
+  } else if (return.method == "parents") {
+    url <- paste0(url, "list.json?name=", name, "&rel=all_parents")
+  } else if (return.method == "auto") {
+    url <- paste0(url, "auto.json?name=", name, "&limit=", limit)
+  } else {
+    stop("Invalid return.method specified")
+  }
+  res <- fromJSON(url)
+  return (res)
+}
 
-test.query <- "http://paleobiodb.org/data1.1/taxa/single.json?name=Dascillidae&show=attr"
-fromJSON(test.query)
+# Test
+taxaNames(name = "homo sapiens", return.method = 'auto')
