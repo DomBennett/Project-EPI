6346fc15e1d098678a918cd5e188222048dc1040
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-03-05 21:14:44 +0000

    Separated data parsing from measures calculations

--- 1_measures.R
+++ 1_measures.R
@@ -17,37 +17,12 @@ if (!file.exists (output.dir)) {
 
 ## Import data (not automated...)
 cat ("Importing data ...\n")
-# Ladybird
-#data <- read.nexus.data (file.path (input.dir, "ladybird_matrix.nex"))
-#data <- lapply (data, function (x) x[2254:2365])
-#chars <- matrix (unlist (data), nrow = length (data), byrow = TRUE)
-#rownames (chars) <- names (data)
-#chars[chars == "?"] <- rep (NA, sum (chars == "?"))
-#phylo <- read.nexus (file.path (input.dir, "ladybird_tree.nex"))[[1]]
-# Mammal (http://esapubs.org/archive/ecol/e090/184/)
-data <- read.delim (file.path (input.dir, "panTHERIA.txt"), na.strings = -999,
-                                        stringsAsFactors = FALSE)
-chars <- data[ ,- c (1:5,36:55)]
-for (i in 1:ncol (chars)) {
-  temp.chars <- chars[!is.na (chars[ , i]),i]
-  if (length (unique (temp.chars)) > 10) {
-    chars[!is.na (chars[ , i]),i] <- cut(temp.chars, 10)
-  }
-}
-rownames (chars) <- data[ ,"MSW93_Binomial"]
-phylo <- read.tree (file.path (input.dir, "bininda.txt"))
-phylo$tip.label <- sub ("_", " ", phylo$tip.label)
-chars <- chars[rownames (chars) %in% phylo$tip.label, ]
-if (!any (phylo$tip.label %in% rownames (chars))) {
-  phylo <- drop.tip (phylo,
-                     tip = phylo$tip.label[!phylo$tip.label %in% rownames (chars)])
-}
-if (!all (c (phylo$tip.label %in% rownames (chars),
-             rownames (chars) %in% phylo$tip.label))) {
-  stop ("All labels do not match between data and phylogeny.")
-}
+load (file.path(input.dir, "mammal.RData")) # or ladybird
+phylo <- data[["phylo"]]
+chars <- data[["chars"]]
+rm (data)
 # start with mangeable size
-#phylo <- drop.tip (phylo, sample (phylo$tip.label, length (phylo$tip.label) - 10))
+#phylo <- drop.tip (phylo, sample (phylo$tip.label, length (phylo$tip.label) - 1000))
 #chars <- chars[phylo$tip.label,]
 if (!is.binary.tree (phylo)) {
   phylo <- multi2di (phylo)
@@ -60,9 +35,9 @@ cat ("Calculating edge changes ...\n")
 phylo <- calcEdgeChanges (phylo, reconstruction.obj)
 #plotEdgeChanges (phylo, by.char = TRUE)
 cat ("Calculating LFI measures ...\n")
-lfi.data <- calcLFI (phylo)
+measures <- calcLFIMeasures (phylo)
 
 ## Output LFI.data
 cat ("Outputting ...\n")
-write.csv (x = lfi.data, file = file.path (output.dir, "measures.csv"), row.names = FALSE)
+write.csv (x = measures, file = file.path (output.dir, "measures.csv"), row.names = FALSE)
 save (phylo, file = file.path (output.dir, "PhyloMeasures.RData"))
