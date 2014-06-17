9e4dbe375a5199a4537768488dd639c25f4b86bc
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-06-17 22:20:58 +0100

    Added extinction

--- cladeSuccessThroughTime.R
+++ cladeSuccessThroughTime.R
@@ -17,42 +17,15 @@ countChildren <- function (tree) {
   # Count the number of children for every node
   .count <- function (node.label) {
     node <- which (tree$node.label == node.label)
-    length (getChildren(tree, node))
+    node <- length (tree$tip.label) + node
+    length (getChildren (tree, node))
   }
   # all internal nodes
-  node.labels <- paste0 ('n', 1:tree$Nnode)
+  node.labels <- tree$node.label
   res <- mdply (.data = data.frame (node.label = node.labels),
                 .fun = .count)
   colnames (res) <- c ('node', 'n.children')
   res
-}
-
-growTree <- function (iterations) {
-  # an equal rates markov model:
-  # choose a species at random
-  # simulate it speciating by adding 1 to all tip edges
-  # adding a new tip at a new node in the random species' connecting
-  # edge at 1 time step ago
-  run <- function (iteration) {
-    # choose a species at random
-    tip.edges <- which (tree$edge[, 2] %in% 1:length (tree$tip.label))
-    #probs <- tree$edge.length[tip.edges] # weight by branch length?
-    random.edge <- sample (x = tip.edges, size = 1)
-    new.node.label <- paste0 ('n', tree$Nnode + 1)
-    new.tip.label <- paste0 ('t', length (tree$tip.label) + 1)
-    # grow tip.edges by 1
-    tree$edge.length[tip.edges] <- tree$edge.length[tip.edges] + 1
-    # new node.age is always 1 -- 1 time step ago.
-    node.age <- 1
-    # add new tip at random edge
-    tree <<- addTip (tree = tree, edge = random.edge,
-                    tip.name = new.tip.label,
-                    node.age = node.age,
-                    node.label = new.node.label)
-    tree
-  }
-  m_ply (.data = (iteration = 1:iterations), .fun = run)
-  tree
 }
 
 reformat <- function (clade.performance) {
@@ -90,22 +63,34 @@ reformat <- function (clade.performance) {
   res
 }
 
-plotSuccess <- function (res, section = 'all') {
+plotSuccess <- function (res, section = 'all',
+                         thinning = 'all') {
   # Plot as separate lines for the success of each
   # clade through time
   # section = 'all' -- for all clads
   # else do for bottom number
+  # thinning allows only a proportion of the
+  # clade results to be plotted
   if (section != 'all') {
     top <- ncol (res)
     bottom <- top - section
     res <- res[bottom:top]
+  }
+  if (thinning != 'all') {
+    # choose those to plot by biasing towards the
+    # older ones -- makes it easier to see
+    # too many young ones
+    random.is <- sample (1: ncol (res),
+                         thinning, prob = 1/1: ncol (res))
+    res <- res[ ,random.is]
   }
   n.clades <- ncol (res)
   # re-use the first 3 rainbow colours
   # red, green and blue
   nreps <- ceiling (n.clades/3)
   cols <- rep (rainbow (3, alpha = 0.7), nreps)
-  plot (res[,1], col = NULL, ylab = 'N', xlab = 'Time')
+  plot (res[,1], col = NULL, ylab = 'N', xlab = 'Time',
+        main = 'Clade success through time')
   for (i in 1:n.clades) {
     non.zero <- res [,i] != 0
     y <- res[non.zero, i]
@@ -114,25 +99,70 @@ plotSuccess <- function (res, section = 'all') {
   }
 }
 
+growTree <- function (iterations, birth = 0.6, death = 0.4) {
+  add <- function () {
+    # an equal rates markov model:
+    # choose a species at random
+    # simulate it speciating by adding 1 to all tip edges
+    # adding a new tip at a new node in the random species' connecting
+    # edge at 1 time step ago
+    # choose a species at random
+    # grow tip.edges by 1
+    tip.edges <- which (tree$edge[, 2] %in%
+                          1:length (tree$tip.label))
+    tree$edge.length[tip.edges] <-
+      tree$edge.length[tip.edges] + 1
+    #probs <- tree$edge.length[tip.edges] # weight by branch length?
+    random.edge <- sample (x = tip.edges, size = 1)
+    new.node.label <- paste0 ('n', max.node + 1)
+    new.tip.label <- paste0 ('t', length (tree$tip.label) + 1)
+    # new node.age is always 1 -- 1 time step ago.
+    node.age <- 1
+    # add new tip at random edge
+    tree <<- addTip (tree = tree, edge = random.edge,
+                     tip.name = new.tip.label,
+                     node.age = node.age,
+                     node.label = new.node.label)
+    max.node <<- max.node + 1
+  }
+  drop <- function () {
+    to.drop <- sample (tree$tip.label, 1)
+    tree <<- drop.tip (tree, to.drop)
+  }
+  run <- function (iteration) {
+    add.bool <- sample (c (TRUE, FALSE), size = 1,
+                        prob = c (birth, death))
+    if (add.bool) {
+      add ()
+    } else if (length (tree$tip.label) > 2) {
+      drop ()
+    } else {
+      print ('Drop species -- but too few species in tree. Skipping.')
+    }
+  }
+  m_ply (.data = (iteration = 1:iterations), .fun = run)
+  tree
+}
+
 # parameters
-time.steps <- 1000
+time.steps <- 10000
 intervals <- 10
+max.node <- 1 # global counter of nodes
 # seed tree of two species
 tree <- stree (2)
 # add lengths and labels
 tree$edge.length <- c (1,1)
 # N.B. ape assumes node labels only correspond to internal nodes
 #  MoreTreeTools assumes they correspond to all nodes
-tree$node.label <- c ('t1', 't2', 'n1')
+tree$node.label <- 'n1'
 iterations <- time.steps/intervals
 clade.performance <- list ()
 for (i in 1:iterations) {
-  tree <- growTree (intervals)
-  #plot (tree)
   print (i)
+  tree <- growTree (intervals, birth = 3, death = 1)
   current.success <- countChildren (tree)
   clade.performance <- c (clade.performance,
                           list (current.success))
 }
 res <- reformat (clade.performance)
-plotSuccess (res, section = 980)
+plotSuccess (res)
