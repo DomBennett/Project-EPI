cd1dea2aa68fae67d26d5bea45d997128bfe5072
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-04-20 19:35:46 +0100

    Updated

--- run.R
+++ run.R
@@ -1,5 +1,4 @@
 source('parameters.R')
 cat('Running all stages for [', stdy_grp, '] ....\n', sep='')
-source('1_mesurables.R')
-source('2_epi.R')
-source('2_epi.R')
+source(file.path('stages', '1_measurables.R'),local=TRUE)
+source(file.path('stages', '2_epi.R'), local=TRUE)
