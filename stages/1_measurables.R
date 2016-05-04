13a94f70de08036b443b9897145f9b4906b9f8b3
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-05-04 16:37:43 +0100

    Shrink to 0.99

--- stages/1_measurables.R
+++ stages/1_measurables.R
@@ -32,7 +32,7 @@ rm(data)
 
 # PROCESS
 cat("Reducing character matrix ...\n")
-chars <- reduceChrctrMtrx(chars, pcut=0.95)
+chars <- reduceChrctrMtrx(chars, pcut=0.99)
 cat("Estimating ancestral node states ...\n")
 reconstruction.obj <- parsimonyReconstruction(chars, phylo)
 cat("Calculating edge changes ...\n")
