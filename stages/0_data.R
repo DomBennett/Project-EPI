cbc9a393a166777a96c411604383a9d0f38aa3fe
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-05-22 18:15:17 +0100

    Updated

--- stages/0_data.R
+++ stages/0_data.R
@@ -1,73 +1,69 @@
 # DATA WRANGLING FOR 1_CHNG
 
-# LIBS
+# START
+cat(paste0('\nStage `data` started at [', Sys.time(), ']\n'))
+
+# FUNCTIONS
 library(ape)
-source (file.path('tools', "data_tools.R"))
+source(file.path('tools', "data_tools.R"))
 
 # DIRS
-output.dir <- "0_data"
-input.dir <- file.path(output.dir, "raw")
+tree_dir <- file.path('0_data', "trees")
+chars_dir <- file.path('0_data', "chars")
 
-# DATA -- Mammal ()
-data <- read.delim (file.path (input.dir, "panTHERIA.txt"), na.strings = -999,
+# MAMMALS
+cat('Working on mammals ....\n')
+data <- read.delim(file.path(chars_dir, "panTHERIA.txt"), na.strings = -999,
                     stringsAsFactors = FALSE)
-pantheria <- data[ ,- c (1:5,36:55)]
-for (i in 1:ncol (pantheria)) {
-  temp.chars <- pantheria[!is.na (pantheria[ , i]),i]
-  if (length (unique (temp.chars)) > 10) {
-    pantheria[!is.na (pantheria[ , i]),i] <- cut(temp.chars, 10)
+pantheria <- data[ ,- c(1:5,36:55)]
+for(i in 1:ncol(pantheria)) {
+  temp.chars <- pantheria[!is.na(pantheria[ , i]),i]
+  if(length(unique(temp.chars)) > 10) {
+    pantheria[!is.na(pantheria[ , i]),i] <- cut(temp.chars, 10)
   }
 }
-rownames (pantheria) <- data[ ,"MSW93_Binomial"]
-rownames (pantheria) <- sub (" ", "_", rownames (pantheria))
+rownames(pantheria) <- data[ ,"MSW93_Binomial"]
+rownames(pantheria) <- sub(" ", "_", rownames(pantheria))
 file <- "morpho_matrix_forR.nex"
-oleary <- readNexusData (file.path (input.dir, file))
-chars <- merge (oleary, pantheria, by = 0, all = TRUE)
-rownames (chars) <- c (rownames (oleary) [!rownames (oleary) %in% rownames (pantheria)], rownames (pantheria))
-phylo <- read.tree (file.path (input.dir, "bininda.txt"))
-clades <- MoreTreeTools::getClades(phylo)
-children <- list()
-children[clades$clade.node] <- gsub("_", " ", clades$clade.children)
-#clade_labels <- paste0('n', 1:(length(phylo$tip.label) + phylo$Nnode))
-clade_labels <- MoreTreeTools::getNodeLabels(phylo, all=FALSE, cache=TRUE,
-                                             parent="Mammalia")
-clade_labels <- paste0(clade_labels, '_', 1:phylo$Nnode)  # prevent duplicates
-clade_labels <- c(gsub("_", " ", phylo$tip.label), clade_labels)
-phylo$clade_labels <- clade_labels
-chars <- chars[rownames (chars) %in% phylo$tip.label, ]
-# missing data
-#sum(is.na (chars))*100/(nrow (chars) * ncol (chars))
-data <- list (phylos = list(phylo), chars = chars, children=children)
-save (data, file = file.path (output.dir, "mammal.RData"))
+oleary <- readNexusData(file.path(chars_dir, file))
+chars <- merge(oleary, pantheria, by = 0, all = TRUE)
+rownames(chars) <- c(rownames(oleary) [!rownames(oleary) %in% rownames(pantheria)], rownames(pantheria))
+tree <- read.tree(file.path(tree_dir, "bininda_mammalia.tre"))
+chars <- chars[rownames(chars) %in% tree$tip.label, ]
+clades_phylo <- MoreTreeTools::getClades(tree)
+data <- list(tree=tree, chars=chars, clades_phylo=clades_phylo)
+save(data, file=file.path(chars_dir, "mammal.RData"))
+prep <- signif(mean(colSums(!is.na(chars)))/length(tree$tip.label), 3)
+cat('Done. Found [', ncol(chars), '] characters each on average representing [', 
+    prep, '%] of all tips\n', sep="")
 
-# DATA -- Birds
+# BIRDS
+cat('Working on birds ....\n')
 file <- "X1228_Morphology Matrix_morphobank.nex"
-livezy <- readNexusData(file.path (input.dir, file))
-phylos <- read.tree(file.path(input.dir, 'jetz_aves.tre'))
-for(i in 1:length(phylos)) {
-  clades <- MoreTreeTools::getClades(phylos[[i]])
-  children <- list()
-  children[clades$clade.node] <- gsub("_", " ", clades$clade.children)
-  clade_labels <- MoreTreeTools::getNodeLabels(phylos[[i]], all=FALSE, cache=TRUE,
-                                               parent="Aves")
-  clade_labels <- paste0(clade_labels, '_', 1:phylos[[i]]$Nnode)  # prevent duplicates
-  clade_labels <- c(gsub("_", " ", phylos[[i]]$tip.label), clade_labels)
-  phylos[[i]]$clade_labels <- clade_labels
-}
+livezy <- readNexusData(file.path(chars_dir, file))
+trees <- read.tree(file.path(tree_dir, 'jetz_aves.tre'))
+trees <- trees[sample(1:length(trees), 1000)]
+tree <- consensus(trees)  # strict consensus tree
+clades_phylo <- MoreTreeTools::getClades(tree)
 # character data is for whole groups
 # use character matching to assign the same value to memebers of the same group
 # NAs for missing taxa
-# use last phylo in loop
-p <- phylos[[1]]
-livezy_mod <- matrix(data=NA, nrow=length(p$tip.label), ncol=ncol(livezy))
-rownames(livezy_mod) <- p$tip.label
+# use last tree in loop
+livezy_mod <- matrix(data=NA, nrow=length(tree$tip.label), ncol=ncol(livezy))
+rownames(livezy_mod) <- tree$tip.label
 for(i in 1:nrow(livezy)) {
-  mtchs <- which(grepl(rownames(livezy)[i], p$tip.label))
+  mtchs <- which(grepl(rownames(livezy)[i], tree$tip.label))
   if(length(mtchs) > 1) {
     for(j in mtchs) {
-      livezy_mod[p$tip.label[j], ] <- livezy[i, ]
+      livezy_mod[tree$tip.label[j], ] <- livezy[i, ]
     }
   }
 }
-data <- list (phylos = phylos, chars = livezy_mod, children=children)
-save (data, file = file.path (output.dir, "bird.RData"))
+data <- list(tree=tree, chars=livezy_mod, clades_phylo=clades_phylo)
+save(data, file = file.path(chars_dir, "bird.RData"))
+prep <- signif(mean(colSums(!is.na(livezy_mod)))/length(tree$tip.label), 3)
+cat('Done. Found [', ncol(livezy_mod), '] characters each on average representing [', 
+    prep, '%] of all tips\n', sep="")
+
+# END
+cat(paste0('\nStage `data` finished at [', Sys.time(), ']\n'))
