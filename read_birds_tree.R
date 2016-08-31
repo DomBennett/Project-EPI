80aa083ced514ce03282263a0368459079c97245
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-08-31 11:38:25 +0100

    Update read bird trees

--- read_birds_tree.R
+++ read_birds_tree.R
@@ -2,7 +2,6 @@
 # let's read each tree in a loop and see if we can diagnose the problem
 library(treeman)
 file <- '0_data/trees/birds.tre'
-file <- "~/Desktop/birds.tre"
 cat('Reading in trstrs...\n', sep="")
 trstrs <- scan(file, what="raw", quiet=TRUE)
 cat("Found [", length(trstrs), "] strs.\n", sep="")
@@ -13,7 +12,7 @@ if(!file.exists("bird_trees")) {
 }
 for(i in 1:length(trstrs)) {
   cat(i, ", ", sep="")
-  tree <- treeman:::.readTree(trstrs[i], TRUE)
+  tree <- treeman:::.readTree(trstrs[i], update=FALSE)
   save(tree, file=file.path("bird_trees", paste0("tree_", i, ".RData")))
   trees[[i]] <- tree
 }
@@ -21,5 +20,3 @@ cat("\nConverting to TreeMen ....\n")
 trees <- as(trees, 'TreeMen')
 cat("\nSaving ....\n")
 save(trees, file="birds_trees.RData")
-
-eds <- calcFrPrp(tree, tree['tips'])
