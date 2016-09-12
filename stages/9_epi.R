2c19ff95fbf55673df35b16041562fb48a44a756
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-09-12 19:13:14 +0100

    EPI updates

--- stages/9_epi.R
+++ stages/9_epi.R
@@ -16,91 +16,57 @@ source(file.path("tools", "epi_tools.R"))
 source(file.path('tools', 'node_obj_tools.R'))
 
 # DIRS
-input_dir <- "8_cntrst_chng"
+cc_dir <- "8_cntrst_chng"
+tt_dir <- "7_timetree"
 output_dir <- '9_epi'
 if (!file.exists(output_dir)) {
   dir.create(output_dir)
 }
 output_file <- file.path(output_dir, "res.RData")
-input_file <- file.path(input_dir, "res.RData")
+cc_file <- file.path(cc_dir, "res.RData")
+tt_file <- file.path(tt_dir, "res.RData")
 
-# INPUT
-load(input_file)
-
-# GET SSTR CANDIDATES
-# ensure sisters are accounted for in order to test autocorrelation
-cnddts <- cnddts[!duplicated(cnddts)]
-cnddts <- unique(unlist(sapply(cnddts,
-                               function(x) c(x, node_obj[[x]][['sstr']]))))
-
-# GENERATE EPI DATAFRAME
-cat("Generating EPI dataframe.... ")
-nms <- tmsplts <- cntrst_ns <- cntrst_chngs <- chngs <- eds <- ns <- rep(NA, length(cnddts))
-for(i in 1:length(cnddts)) {
-  tmsplt <- node_obj[[cnddts[i]]][["tmsplt"]]
-  # if no tmsplt, use sstrs
-  if(is.null(tmsplt)) {
-    sstrs <- node_obj[[cnddts[i]]][['sstr']]
-    if(length(sstrs) > 0) {
-      sstr_tmsplts <- sapply(sstrs, function(x) {
-        res <- NA
-        if(!is.null(node_obj[[x]][['tmsplt']])) {
-          res <- node_obj[[x]][['tmsplt']]
-        }
-        res})
-      tmsplt <- mean(sstr_tmsplts, na.rm=TRUE)
-    }
-  }
-  cntrst_n <- node_obj[[cnddts[i]]][["cntrst_n"]]
-  if(!is.null(tmsplt) & !is.null(cntrst_n)) {
-    tmsplts[i] <- tmsplt
-    cntrst_ns[i] <- cntrst_n
-    ns[i] <- length(node_obj[[cnddts[i]]][['kids']])
-    nms[i] <- node_obj[[cnddts[i]]][["nm"]][["scientific name"]]
-  }
-  if(!is.null(node_obj[[cnddts[i]]][['ed']])) {
-    eds[i] <- node_obj[[cnddts[i]]][['ed']]
-  }
-  if(!is.null(node_obj[[cnddts[i]]][["cntrst_chng"]])) {
-    cntrst_chngs[i] <- node_obj[[cnddts[i]]][["cntrst_chng"]]
-    chngs[i] <- node_obj[[cnddts[i]]][["chng"]]
-  }
-}
-bool <- !is.na(cntrst_ns)
-cntrst_ns <- cntrst_ns[bool]
-tmsplts <- tmsplts[bool]
-cnddts <- cnddts[bool]
-nms <- nms[bool]
-cntrst_chngs <- cntrst_chngs[bool]
-chngs <- chngs[bool]
-eds <- eds[bool]
-ns <- ns[bool]
-epi <- data.frame(nm=nms, txid=cnddts, tmsplt=tmsplts, cntrst_n=cntrst_ns,
-                  chng=chngs, ed=eds, cntrst_chng=cntrst_chngs, n=ns,
-                  stringsAsFactors=FALSE)
+# CLADE DATA FRAME
+cat("Generating clade dataframe.... ")
+load(cc_file)
+cnddts <- ls(node_obj)
+cld_data <- genDataframe(cnddts, node_obj)
+load(tt_file)
+cld_data <- rbind(cld_data, genDataframe(cnddts, tt_obj))
+rm(tt_obj, node_obj)
 cat("Done.\n")
 
 # CLEAN-UP
 # some data points don't make sense, reasons as yet unknown
-epi <- epi[epi[['cntrst_n']] != Inf, ]  # two instances of this
+cld_data <- cld_data[cld_data[['cntrst_n']] != Inf, ]  # two instances of this
 
 # CALCULATE EPIS
 cat("Calculating EPIs .... ")
-epi$epi <- log((epi[['cntrst_chng']] + epi[['cntrst_n']]) / epi[['tmsplt']])
-epi$pepi <- log(epi[['cntrst_n']] / epi[['tmsplt']])
+cld_data$epi <- log((cld_data[['cntrst_chng']] + cld_data[['cntrst_n']]) / cld_data[['tmsplt']])
+cld_data$pepi <- log(cld_data[['cntrst_n']] / cld_data[['tmsplt']])
 cat("Done.\n")
 
 # PLOTS
 plotEPI(epi, n=25)
 
+
+
+
 pdf(file.path(output_dir, "res.pdf"))
-ggplot(data=epi, aes(x=log(tmsplt), y=log(cntrst_n), colour=pepi)) +
+ggplot(data=p_data, aes(x=log(tmsplt), y=log(cntrst_n), colour=pepi)) +
   geom_point() + theme_bw()
 ggplot(data=epi, aes(x=pepi, y=log(ed))) +
   geom_point() + theme_bw()
 ggplot(data=epi, aes(x=pepi, y=chng)) +
   geom_point() + theme_bw()
 dev.off()
+
+# EPI and PEPI
+cor.test(cld_data$pepi, cld_data$epi)  # correlate strongly, R = ~0.7
+# this is what you would expect though, how much does change by itself correlate with pepi
+pull <- !is.na(cld_data$cntrst_chng)
+cor.test(cld_data$pepi[pull], cld_data$cntrst_chng[pull])
+# TODO: workout why change has NA, Nan and 1
 
 # ADDING TAXONOMIC INFO
 txids <- ls(node_obj)
--- tools/epi_tools.R
+++ tools/epi_tools.R
@@ -1,4 +1,59 @@
 
+genDataframe <- function(cnddts, node_obj) {
+  # generate clade data frame for clades of interest
+  # ensure sisters are accounted for in order to test autocorrelation
+  cnddts <- cnddts[!duplicated(cnddts)]
+  cnddts <- unique(unlist(sapply(cnddts,
+                                 function(x) c(x, node_obj[[x]][['sstr']]))))
+  nms <- tmsplts <- cntrst_ns <- cntrst_chngs <-
+    chngs <- eds <- ns <- rep(NA, length(cnddts))
+  # loop through candidate IDs and extract info
+  for(i in 1:length(cnddts)) {
+    tmsplt <- node_obj[[cnddts[i]]][["tmsplt"]]
+    # if no tmsplt, use sstrs
+    if(is.null(tmsplt)) {
+      sstrs <- node_obj[[cnddts[i]]][['sstr']]
+      if(length(sstrs) > 0) {
+        sstr_tmsplts <- sapply(sstrs, function(x) {
+          res <- NA
+          if(!is.null(node_obj[[x]][['tmsplt']])) {
+            res <- node_obj[[x]][['tmsplt']]
+          }
+          res})
+        tmsplt <- mean(sstr_tmsplts, na.rm=TRUE)
+      }
+    }
+    cntrst_n <- node_obj[[cnddts[i]]][["cntrst_n"]]
+    if(!is.null(tmsplt) & !is.null(cntrst_n)) {
+      tmsplts[i] <- tmsplt
+      cntrst_ns[i] <- cntrst_n
+      ns[i] <- length(node_obj[[cnddts[i]]][['kids']])
+      nms[i] <- node_obj[[cnddts[i]]][["nm"]][["scientific name"]]
+    }
+    if(!is.null(node_obj[[cnddts[i]]][['ed']])) {
+      eds[i] <- node_obj[[cnddts[i]]][['ed']]
+    }
+    if(!is.null(node_obj[[cnddts[i]]][["cntrst_chng"]])) {
+      cntrst_chngs[i] <- node_obj[[cnddts[i]]][["cntrst_chng"]]
+      chngs[i] <- node_obj[[cnddts[i]]][["chng"]]
+    }
+  }
+  bool <- !is.na(cntrst_ns)
+  cntrst_ns <- cntrst_ns[bool]
+  tmsplts <- tmsplts[bool]
+  cnddts <- cnddts[bool]
+  nms <- nms[bool]
+  cntrst_chngs <- cntrst_chngs[bool]
+  chngs <- chngs[bool]
+  eds <- eds[bool]
+  ns <- ns[bool]
+  cld_data <- data.frame(nm=nms, txid=cnddts, tmsplt=tmsplts, cntrst_n=cntrst_ns,
+                         chng=chngs, ed=eds, cntrst_chng=cntrst_chngs, n=ns,
+                         stringsAsFactors=FALSE)
+  cld_data
+}
+
+
 normalise <- function(xs) {
   (xs - min(xs, na.rm=TRUE)) /
     ( max(xs, na.rm=TRUE) - min(xs, na.rm=TRUE) )
@@ -29,13 +84,20 @@ EPIChecker <- function (metrics, cut) {
   text (labels = '<-- Living fossils', x=cutoff, y=nrow(metrics)/100, cex = 0.8)
 }
 
-plotEPI <- function(epi, n=20) {
-  ordrd_top <- order(epi[['pepi']])[1:n]
-  val <- c(epi[['pepi']][ordrd_top],
-           epi[['epi']][ordrd_top],
-           epi[['ed']][ordrd_top])
+plotEPI <- function(epi, n=100) {
+  pepi_z <- (cld_data[['pepi']] - mean(cld_data[['pepi']], na.rm=TRUE)) /
+    sd(cld_data[['pepi']], na.rm=TRUE)
+  epi_z <- (cld_data[['epi']] - mean(cld_data[['epi']], na.rm=TRUE)) /
+    sd(cld_data[['epi']], na.rm=TRUE)
+  log_ed <- log(cld_data[['ed']])
+  ed_z <- (mean(log_ed, na.rm=TRUE) - log_ed) /
+    sd(log_ed, na.rm=TRUE)
+  ordrd_top <- order(cld_data[['pepi']])[1:n]
+  val <- c(pepi_z[ordrd_top],
+           epi_z[ordrd_top],
+           ed_z[ordrd_top])
   type <- rep(c('pEPI', 'EPI', 'ED'), each=length(ordrd_top))
-  nms <- epi[['nm']][ordrd_top]
+  nms <- cld_data[['nm']][ordrd_top]
   nm <- rep(nms, 3)
   nm <- factor(nm, levels=nms[length(nms):1])
   p_data <- data.frame(nm, val, type)
